838be2854f333dd10fb0708add9cd5dddc950fee
Billy <nulledroom@gmail.com>
2018-10-28 15:52:07 +0900

    Updated with a login mechanism

--- hotel.c
+++ hotel.c
@@ -6,10 +6,51 @@
 struct User {
     int userid;
     int age;
+    char id[20];
     char gender[6];
     char name[64];
     char password[64];
 };
+
+void read_file(struct User users[]){
+    FILE *fp;
+    int i = 0;
+    int c;
+    fp = fopen("user.txt","r");
+    while(!feof(fp)){
+        c = fgetc(fp);
+        if(c == '\n'){
+            ++i;
+        }
+    }
+    int j = 0;
+    rewind(fp);
+    while(j != i - 1){
+        fscanf(fp, "%s %s %s", users[j].userid, users[j].name, users[j].password);
+        ++j;
+    }
+    fclose(fp);
+}
+int isLogged = 0;
+void login(char userid[], char password[]){
+    char temp_id[64];
+    char temp_name[64];
+    char temp_password[64];
+    FILE *fp;
+    fp = fopen("user.txt","r");
+        while(fscanf(fp, "%s %s %s", temp_id, temp_name, temp_password) != EOF){
+            if(strcmp(temp_id, userid) == 0){
+                if(strcmp(temp_password, password) == 0){
+                    // printf("Passed");
+                    isLogged = 1;
+                }
+            }
+        }
+}
+
+struct User users[64];
+struct User CurrentUser;
+char userInput1;
 
 // count number of records to generate userid
 int countRecords(char *file){
@@ -24,7 +65,6 @@ int countRecords(char *file){
     }
     return lines + 1;
 }
-
 // register user by adding records to the file
 char userReg(int id, char name[], char password[]){
     FILE *fptr;
@@ -34,24 +74,12 @@ char userReg(int id, char name[], char password[]){
         printf("Error!");   
         exit(1);             
     }
-    fprintf(fptr,"%d|%s|%s\n",id, name, password);
+    fprintf(fptr,"%d %s %s\n",id, name, password);
     fclose(fptr);
     printf("----------------------------------------------\n");
     printf("       \nYou have successfully registered!\n\n");
     printf("----------------------------------------------\n");
 }
-
-int userLogin(char username[], char password[]){
-    
-}
-
-struct User CurrentUser;
-
-int userLogged = 0;
-char userInput;
-int isLogged = 0;
-int isAdmin = 0;
-
 void mainScreen(char a[], char b[], char c[]){
     printf("##############################################\n");
     printf("#              Suite  Bliss                  #\n");
@@ -63,21 +91,18 @@ void mainScreen(char a[], char b[], char c[]){
     printf("----------------------------------------------\n");
     printf("----------------------------------------------\n");
 }
-
-
 void msgInfo(char message[]){
     printf("\n----------------------------------------------\n");
     printf("%s                        \n", message);
     printf("----------------------------------------------\n");
 }
 
-
 int main(void){   
     mainScreen("Registration","Login","Exit");
     while(true){
         printf("Please enter an option: ");
-        scanf("%d", &userInput);
-        switch(userInput){
+        scanf("%d", &userInput1);
+        switch(userInput1){
             case 1: //register();
                 msgInfo("Please complete the details below to register.");
                 CurrentUser.userid = countRecords("user.txt");
@@ -91,9 +116,16 @@ int main(void){
             case 2: //login();
                 msgInfo("Please enter your USER-ID and PASSWORD.");
                 printf("UserID: ");
-                scanf("%d", &CurrentUser.userid);
+                scanf("%s", &CurrentUser.id);
                 printf("Password: ");
                 scanf("%s", &CurrentUser.password);
+                login(CurrentUser.id, CurrentUser.password);
+                if(isLogged == 1){
+                    printf("Welcome to the booking system.");
+                }else{
+                    printf("Invalid Credentials.\n\n");
+                    continue;
+                }
                 break;
             case 3: 
                 msgInfo("System has been exited successfully!");
--- read.c
+++ read.c
@@ -2,21 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void) {
-    char username[64];
-    char password[64];
-    FILE *ptr_file;
-    char buf[1000];
+struct account {
+    char id[20];
+    char name[20];
+    char password[20];
+};
 
-    ptr_file = fopen("user.txt", "r");
-    if(!ptr_file)
-        return 1;
-    while(fgets(buf, 1000, ptr_file)!= NULL){
-        printf(fscanf(ptr_file,"%s %s %s %s", &username, &password));
+struct account accounts[10];
+void read_file(struct account accounts[]){
+    FILE *fp;
+    int i = 0;
+    int c;
+    fp = fopen("user.txt","r");
+    while(!feof(fp)){
+        c = fgetc(fp);
+        if(c == '\n'){
+            ++i;
+        }
     }
+        int j = 0;
+        rewind(fp);
+
+        while(j != i - 1){
+            fscanf(fp, "%s %s %s", accounts[j].id, accounts[j].name, accounts[j].password);
+            ++j;
+        }
+    }
+
+
+int main(void) {
+    int iii = 2;
+    char * ii;
+    sprintf(ii, "%d", iii);
+    const char * user = "Billy"; 
+    read_file(accounts);
+    if(strcmp(ii, accounts[0].id) == 0){
+        printf("Success1");
+        if(strcmp(user, accounts[0].name) == 0){
+            printf("Success2");
+        }
+    }
+    // printf("%s", ii);
+    // printf("%s", accounts[0].id);
+    // char username[64];
+    // char password[64];
+    // FILE *ptr_file;
+    // char buf[1000];
+
+    // ptr_file = fopen("user.txt", "r");
+    // if(!ptr_file)
+    //     return 1;
+    // while(fgets(buf, 1000, ptr_file)!= NULL){
+    //     printf(fscanf(ptr_file,"%s %s %s %s", &username, &password));
+    // }
         
 
-        fclose(ptr_file);
-        return 0;
+    //     fclose(ptr_file);
+    //     return 0;
     
 }
